0386ed74303cb86c6ecdfb5f00fc392195199e5d
aborodavkin <algebord@gmail.com>
2016-07-10 14:04:25 +0300

    Заменил первоначальный код

--- drawman.py
+++ drawman.py
@@ -1,51 +1,71 @@
-from turtle import Turtle
+from turtle import *
+from math import *
+t = Turtle()
+x = 0
+y = 0
+t.speed(100)
 
+def drawman_scale(scale):
+    global _drawman_scale
+    _drawman_scale = scale
 
-def init_drawman():
-    global t, x_current, y_current
-    t = Turtle()
+def calculate_angle(dx, dy):
+    if dx > 0:
+        return atan(dy/dx)*180/pi
+    elif dx < 0:
+        if dy > 0:
+            return 180 + atan(dy/dx)*180/pi
+        else:
+            return -180 + atan(dy/dx)*180/pi
+    elif dx == 0:
+        if dy > 0:
+            return 90
+        else:
+            return -90
+
+def shift(dx, dy):
+    global x, y
+    x += dx
+    y += dy
+    angle = calculate_angle(dx, dy)
+    length = (dx**2 + dy**2)**0.5
+    t.left(angle)
+    t.forward(length)
+    t.right(angle)
+
+def goto(x1, y1):
+    shift(x1 - x, y1 - y)
+    
+def coordinate_lines(x0 = 0, y0 = 0):
+    current_color = t.color()
+    t.color('blue')
+    dx = window_width()/2
+    dy = window_height()/2
+    # x line
+    goto(-dx, y0)
+    pendown()
+    goto(+dx, y0)
+    goto(dx - 10, y0 + 10)
+    goto(+dx, y0)
+    goto(dx - 10, y0 - 10)
+    goto(+dx, y0)
+    penup()
+    # y line
+    goto(x0, -dy)
+    pendown()
+    goto(x0, +dy)
+    goto(x0 - 10, dy - 10)
+    goto(x0, +dy)
+    goto(x0 + 10, dy - 10)
+    goto(x0, +dy)
+    penup()
+    t.color(*current_color)
+
+def penup():
     t.penup()
-    x_current = 0
-    y_current = 0
-    t.goto(x_current, y_current)
 
-def test_drawman():
-    """
-    Тестирование работы Чертёжника
-    :return: None
-    """
-    pen_down()
-    for i in range(5):
-        on_vector(10, 20)
-        on_vector(0, -20)
-    pen_up()
-    to_point(0, 0)
-
-
-def pen_down():
+def pendown():
     t.pendown()
 
-
-def pen_up():
-    t.penup()
-
-
-def on_vector(dx, dy):
-    global x_current, y_current
-    x_current += dx
-    y_current += dy
-    t.goto(x_current, y_current)
-
-
-def to_point(x, y):
-    global x_current, y_current
-    x_current = x
-    y_current = y
-    t.goto(x_current, y_current)
-
-
-init_drawman()
-if __name__ == '__main__':
-    import time
-    test_drawman()
-    time.sleep(10)
+def color(col):
+    t.color(col)
